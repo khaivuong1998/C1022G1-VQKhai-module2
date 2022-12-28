2073db01fb983e9c43caf5af40dcbaca6f41eae5
unknown <khaivuong3110>
2022-12-28 21:49:06 +0700

    bai tap ss18

--- Module_2/src/case_study/controllers/FuramaController.java
+++ Module_2/src/case_study/controllers/FuramaController.java
@@ -7,7 +7,7 @@ public class FuramaController {
         System.out.println("------Menu Furama------");
         System.out.println("1. Employee Management");
         System.out.println("2. Customer Management");
-        System.out.println("3. Customer Management");
+        System.out.println("3. Facility Management");
         System.out.println("4. Booking Management");
         System.out.println("5. Promotion Management");
         System.out.println("6. Exit");
